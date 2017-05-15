aaf5ac201435d34340497d19e4d47c79de7cb035
John Evans <lgastako@gmail.com>
2017-05-15 01:14:32 -0600

    A taste of our own medicine.

--- src/Overture.hs
+++ src/Overture.hs
@@ -8,12 +8,11 @@ module Overture
        , show_
        ) where
 
-import Prelude hiding ( getLine )
-
-import           Data.Text    ( Text )
+import           Prelude                hiding  ( getLine )
+import           Data.Text                      ( Text )
 import qualified Data.Text    as Text
 import qualified Data.Text.IO as TextIO
-import           Safe         ( headMay )
+import           Safe                           ( headMay )
 
 (|>) :: a -> (a -> c) -> c
 (|>) = flip ($)
