4aad54edf3ef1626ca1fb19716c014f404956cff
John Evans <lgastako@gmail.com>
2017-05-15 01:23:15 -0600

    A bit more.

--- src/Longboye/Main.hs
+++ src/Longboye/Main.hs
@@ -1,20 +1,20 @@
 {-# LANGUAGE QuasiQuotes     #-}
 module Longboye.Main ( main ) where
 
-import           Control.Monad         ( when )
+import           Control.Monad                    ( when )
 import qualified Longboye.Imports      as Imports
-import           System.Environment    ( getArgs )
-import           System.Exit           ( exitSuccess )
-import           System.Console.Docopt ( Docopt
-                                       , argument
-                                       , command
-                                       , docoptFile
-                                       , exitWithUsage
-                                       , getAllArgs
-                                       , isPresent
-                                       , longOption
-                                       , parseArgsOrExit
-                                       )
+import           System.Environment               ( getArgs )
+import           System.Exit                      ( exitSuccess )
+import           System.Console.Docopt            ( Docopt
+                                                  , argument
+                                                  , command
+                                                  , docoptFile
+                                                  , exitWithUsage
+                                                  , getAllArgs
+                                                  , isPresent
+                                                  , longOption
+                                                  , parseArgsOrExit
+                                                  )
 
 patterns :: Docopt
 patterns = [docoptFile|USAGE.txt|]
