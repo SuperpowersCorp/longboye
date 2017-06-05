55ddfd73ff8bec4a1f2823b7eb53c86b2a2f43eb
John Evans <lgastako@gmail.com>
2017-06-04 23:35:38 -0600

    checkpt

---
+++ test/Longboye/Member/Arbitrary.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Longboye.Member.Arbitrary () where
+
+import Data.Text.Arbitrary ()
+import Longboye.Member     ( Member( NamedMember
+                                   , OpMember
+                                   ) )
+import Test.QuickCheck     ( Arbitrary
+                           , arbitrary
+                           )
+
+instance Arbitrary Member where
+  arbitrary = do
+    coin <- arbitrary
+    name <- arbitrary -- TODO: valid names only
+    if coin
+      then arbNamed name
+      else arbOp name
+    where
+      arbNamed name = do
+        b <- arbitrary
+        return $ NamedMember name b
+      arbOp name = do
+        ops <- arbitrary
+        return $ OpMember name ops
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,40 @@
+module Spec
+       ( main
+       , spec
+       ) where
 
-
-import Data.Monoid      ( (<>) )
-import Longboye.Imports ( interactS )
-import Test.Hspec
-
--- import Test.QuickCheck
+import           Data.Monoid                         ( (<>) )
+import           Data.Text                           ( isInfixOf )
+import           Longboye.Import                     ( Import )
+import qualified Longboye.Import           as Import
+import           Longboye.Import.Arbitrary           ()
+import           Longboye.Imports                    ( interactS )
+import           Test.Hspec
+import           Test.QuickCheck                     ( property )
+import qualified Test.QuickCheck           as QC
+import           Test.QuickCheck.Random              ( mkQCGen )
 
 main :: IO ()
-main = hspec $
+main = hspec spec
+
+spec :: Spec
+spec = do
   describe "Imports.interact" $
     it "handles (:<|>)(..) correctly" $ do
-      let sscce = "import Foo ( (:<|>)(..) )"
-      interactS sscce `shouldBe` ("\n\n" <> sscce <> "\n\n")
+      let sscce      = "import Foo ( (:<|>)(..) )"
+          extensions = []
+      interactS extensions sscce `shouldBe` ("\n\n" <> sscce <> "\n\n")
+
+  describe "Imports.format" $
+    it "Never stacks closing parens" $ property $ prop_neverStacksParensAcrossLines
+
+
+prop_neverStacksParensAcrossLines :: Bool -> Bool -> Int -> Int -> Import -> Bool
+prop_neverStacksParensAcrossLines anyQ anyH maxModLen maxAsLen imp =
+  not $ ") )" `isInfixOf` (Import.format anyQ anyH maxModLen maxAsLen imp)
+
+_tmpTest :: IO ()
+_tmpTest =
+  QC.quickCheckWith args prop_neverStacksParensAcrossLines
+  where args = QC.stdArgs{QC.replay = Just (mkQCGen seed, 0)}
+        seed = 99666056
