c120cb67bc06a1f692fbf20a1f9b2296196d08c4
John Evans <lgastako@gmail.com>
2018-02-06 00:42:37 -0500

    DRY up command line parsing.

--- src/Longboye/Main.hs
+++ src/Longboye/Main.hs
@@ -7,28 +7,27 @@ module Longboye.Main ( main ) where
 import qualified Prelude
 import           Longboye.Prelude
 
-import           Longboye.Files                          ( genericClean
-                                                         , genericInteract
-                                                         )
-import qualified Longboye.Imports       as Imports
-import qualified Longboye.ImportsParser as ImportsParser
-import qualified Longboye.Pragmas       as Pragmas
-import qualified Longboye.PragmasParser as PragmasParser
-import           System.Console.Docopt                   ( Docopt
-                                                         , argument
-                                                         , command
-                                                         , docoptFile
-                                                         , exitWithUsage
-                                                         , getAllArgs
-                                                         , isPresent
-                                                         , longOption
-                                                         , parseArgsOrExit
-                                                         )
-import           System.Environment                      ( getArgs )
-import           System.Exit                             ( exitSuccess )
-
--- import qualified Longboye.ModuleStatementParser as ModuleStatementParser
--- import qualified Longboye.ModuleStatements      as ModuleStatements
+import           Longboye.Files                                          ( genericClean
+                                                                         , genericInteract
+                                                                         )
+import qualified Longboye.Imports               as Imports
+import qualified Longboye.ImportsParser         as ImportsParser
+import qualified Longboye.ModuleStatementParser as ModuleStatementParser
+import qualified Longboye.ModuleStatements      as ModuleStatements
+import qualified Longboye.Pragmas               as Pragmas
+import qualified Longboye.PragmasParser         as PragmasParser
+import           System.Console.Docopt                                   ( Docopt
+                                                                         , argument
+                                                                         , command
+                                                                         , docoptFile
+                                                                         , exitWithUsage
+                                                                         , getAllArgs
+                                                                         , isPresent
+                                                                         , longOption
+                                                                         , parseArgsOrExit
+                                                                         )
+import           System.Environment                                      ( getArgs )
+import           System.Exit                                             ( exitSuccess )
 
 patterns :: Docopt
 patterns = [docoptFile|USAGE.txt|]
@@ -48,27 +47,18 @@ main_ argv = do
   when (opts `isPresent` longOption "help") $
     exitWithUsage patterns
 
-  when (opts `isPresent` command "imports") $
-    case opts `getAllArgs` argument "path" of
-      ["-"] -> genericInteract ImportsParser.parseE Imports.cleanText
-      ps
-        | "-" `elem` ps -> panic cannotMixErr
-        | otherwise     -> genericClean ImportsParser.parseE Imports.cleanText ps
-
-  -- when (opts `isPresent` command "modules") $
-  --   case opts `getAllArgs` argument "path" of
-  --     ["-"] -> genericInteract ModuleStatementParser.parseE ModuleStatements.cleanText
-  --     ps
-  --       | "-" `elem` ps -> panic cannotMixErr
-  --       | otherwise     -> genericClean ModuleStatementParser.parseE ModuleStatements.cleanText ps
-
-  when (opts `isPresent` command "pragmas") $
-    case opts `getAllArgs` argument "path" of
-      ["-"] -> genericInteract PragmasParser.parseE Pragmas.cleanText
-      ps
-        | "-" `elem` ps -> panic cannotMixErr
-        | otherwise     -> genericClean PragmasParser.parseE Pragmas.cleanText ps
+  whenRequested opts "imports" ImportsParser.parseE         Imports.cleanText
+  whenRequested opts "modules" ModuleStatementParser.parseE ModuleStatements.cleanText
+  whenRequested opts "pragmas" PragmasParser.parseE         Pragmas.cleanText
 
   where
     version      = "0.0.0.1"
     cannotMixErr = "you cannot mix stdin (-) with files.  try one or the other."
+
+    whenRequested opts cmd parse clean =
+      when (opts `isPresent` command cmd) $
+        case opts `getAllArgs` argument "path" of
+          ["-"] -> genericInteract parse clean
+          ps
+            | "-" `elem` ps -> panic cannotMixErr
+            | otherwise     -> genericClean parse clean ps
