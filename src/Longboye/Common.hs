66798f1e2023c640d61157a038e53b7711e3e5bf
John Evans <lgastako@gmail.com>
2018-10-03 11:48:06 -0600

    Initial fixities fix.

--- src/Longboye/Common.hs
+++ src/Longboye/Common.hs
@@ -4,14 +4,20 @@ module Longboye.Common
   ( parseMode
   ) where
 
-import Longboye.Prelude
+import Longboye.Prelude hiding (Fixity)
 
 import Data.List                       ( nub )
-import Language.Haskell.Exts           ( ParseMode )
+import Language.Haskell.Exts           ( Fixity
+                                       , ParseMode
+                                       , infixl_
+                                       , infixr_
+                                       , infix_
+                                       )
 import Language.Haskell.Exts.Extension
 import Language.Haskell.Exts.Parser    ( baseLanguage
                                        , defaultParseMode
                                        , extensions
+                                       , fixities
                                        , ignoreLanguagePragmas
                                        , parseFilename
                                        )
@@ -19,10 +25,31 @@ import Language.Haskell.Exts.Parser    ( baseLanguage
 parseMode :: FilePath -> [Extension] -> ParseMode
 parseMode path foundExtensions = defaultParseMode
   { baseLanguage          = Haskell2010
-  , ignoreLanguagePragmas = False
   , extensions            = configuredExtensions foundExtensions
+  , fixities              = lensFixities
+  , ignoreLanguagePragmas = False
   , parseFilename         = path
   }
+
+lensFixities :: Maybe [Fixity]
+lensFixities = Just
+  $  infixl_ 9 [ ":>", "<.>", "<.", ".>", "...", "#." ]
+  <> infixl_ 8 [ "^..", "^?", "^?!", "^@..", "^@?", "^@?!", "^.", "^@.", "^#"
+               , ".#", "^!", "^@!"
+               ]
+  <> infixr_ 4 [ "</>~", "<</>~", "<.>~", "<<.>~", "<#~", "#~", "#%~", "<#%~"
+               , "#%%~", ".|.~", ".&.~", "<.|.~", "<.&.~", "%@~", ".~", "+~"
+               , "*~", "-~", "//~", "^~", "^^~", "**~", "&&~", "<>~", "||~"
+               , "%~", "%%@~", "<%@~", "%%~", "<+~", "<*~", "<-~", "<//~"
+               , "<^~", "<^^~", "<**~" ]
+  <> infix_ 4  [ "</>=", "<</>=", "<.>=", "<<.>=", "<#=", "#=", "#%=", "<#%="
+               , "#%%=", ".|.=", ".&.=", "<.|.=", "<.&.=", "%@=", ".=", "+="
+               , "*=", "-=", "//=", "^=", "^^=", "**=", "&&=", "<>=", "||="
+               , "%=", "%%@=", "<%@=", "%%=", "<+=", "<*=", "<-=", "<//="
+               , "<^=", "<^^=", "<**="
+               ]
+  <> infixr_ 2 [ "`zoom`", "`magnify`", "<~", "<<~" ]
+  <> infixl_ 1 ["&", "&~", "<&>", "??"]
 
 configuredExtensions :: [Extension] -> [Extension]
 configuredExtensions foundExtensions = nub
